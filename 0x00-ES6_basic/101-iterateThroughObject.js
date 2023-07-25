afce5e3e85e2e83123d35ea37c2f32fb0681dc3d
Ochoja <danielochoja@yahoo.com>
2023-07-25 07:09:47 +0100

    updated

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,16 @@
+export default function iterateThroughObject(reportWithIterator) {
+  const array = reportWithIterator;
+  let str = '';
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (str === '') {
+      str += array[i];
+    } else if (i === array.length - 1) {
+      str += ` | ${array[i]}`;
+    } else {
+      str += ` | ${array[i]} `;
+    }
+  }
+
+  return str;
+}
