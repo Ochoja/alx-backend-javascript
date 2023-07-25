6a7b59f7e739de6bc0b3513e7a901467028eab5d
Ochoja <danielochoja@yahoo.com>
2023-07-25 06:30:39 +0100

    updated

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
+  let index = 0;
   for (let idx of array) {
     idx = appendString + idx;
+    array[index] = idx;
+    index += 1;
   }
 
   return array;
