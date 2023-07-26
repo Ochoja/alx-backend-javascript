41088fec6373d4e9b7e459c9172dc22243f7edb8
Ochoja <danielochoja@yahoo.com>
2023-07-27 00:52:51 +0100

    updated

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    return {
-      status: 200,
-      body: 'success',
-    };
-  }).then(() => {
-    console.log('Got a response from the API');
-  });
-
-  promise.catch(() => Error());
+  promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => Error());
 }
