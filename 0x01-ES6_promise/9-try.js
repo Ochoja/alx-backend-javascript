cfa3f7d18e71ae207e98f904cf17d25c61794ad7
Ochoja <danielochoja@yahoo.com>
2023-07-27 03:21:03 +0100

    updated

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+export default function (mathFunction) {
+  const queue = [];
+
+  try {
+    queue.push(mathFunction());
+  } catch (error) {
+    queue.push(error);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
