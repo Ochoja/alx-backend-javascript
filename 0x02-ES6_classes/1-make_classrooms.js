66782972e39f3c29d4a27ac6977cb1ab50c5f4a4
Ochoja <danielochoja@yahoo.com>
2023-07-28 14:58:44 +0100

    updated

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+import ClassRoom from "./0-classroom";
+
+function initializeRooms() {
+  arr = [];
+
+  arr.push(ClassRoom(19));
+  arr.push(ClassRoom(20));
+  arr.push(ClassRoom(34));
+
+  return arr;
+}
