258d712b7f140000ce628893b66931d96cd80920
Ochoja <danielochoja@yahoo.com>
2023-07-28 16:12:07 +0100

    updated script

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,16 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    if (typeof name != "string") {
+      throw new TypeError("Name must be a string");
+    } else this._name = name;
+
+    if (typeof length != "number") {
+      throw new TypeError("Length must be a number");
+    } else this._length = length;
+
+    if (typeof students != "object") {
+      throw new TypeError("Students must be an object");
+    } else this._students = students;
   }
 
   // getter methods
