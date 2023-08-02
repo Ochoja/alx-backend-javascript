19385c3878ae0a137fcce2bdd05bfca1b653faae
Ochoja <danielochoja@yahoo.com>
2023-08-02 17:33:38 +0100

    updated

--- 0x03-ES6_data_manipulation/main.js
+++ 0x03-ES6_data_manipulation/main.js
@@ -1,3 +1,7 @@
 import setFromArray from "./6-set.js";
 
-console.log(setFromArray([12, 32, 15, 78, 98, 15]));
+const newSet = setFromArray([12, 32, 15, 78, 98, 15]);
+
+for (let i of newSet) {
+  console.log(i)
+}
