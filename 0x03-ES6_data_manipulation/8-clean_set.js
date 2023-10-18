c216cea1b08edacd38227aa4408b14b925904405
Daniel Ochoja <danielochoja@yahoo.com>
2023-10-18 14:27:48 +0100

    updated

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,21 @@
 export default function cleanSet(newSet, startString) {
   let newString = '';
+  const newArray = [];
   const len = startString.length;
 
   newSet.forEach((word) => {
     // check if word starts with startString
     if (word.startsWith(startString) && startString != '') {
-      newString = newString.concat(word.slice(len), '-'); //concatenate string to newString
+      newArray.push(word.slice(len));
+      //   newString = newString.concat(word.slice(len), '-');
     }
   });
+
+  for (let i = 0; i < newArray.length; i++) {
+    if (newArray[i] != '') {
+      newString = newString.concat(newArray[i], '-');
+    }
+  }
 
   return newString.slice(0, newString.length - 1);
 }
