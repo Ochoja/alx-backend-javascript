6901529aa2a56dddd5455e597fc96a488d9810e3
Daniel Ochoja <danielochoja@yahoo.com>
2024-01-05 04:10:31 +0100

    updated

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,22 +1,15 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor === Building) {
-      throw new Error(`Abstract classes can't be instantiated`);
-    }
     this._sqft = sqft;
-  }
 
-  set sqft(newSqft) {
-    this._sqft = newSqft;
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw Error(
+        'Class extending Building must override evacuationWarningMessage'
+      );
+    }
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error(
-      'Class extending Building must override evacuationWarningMessage'
-    );
   }
 }
