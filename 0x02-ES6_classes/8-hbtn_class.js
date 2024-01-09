28d8e9ab4bbebb1c4b86ec724bc589e8a2b54375
Daniel Ochoja <danielochoja@yahoo.com>
2024-01-09 02:16:52 +0100

    updated

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -4,6 +4,8 @@ export default class HolbertonClass {
     this._location = location;
   }
 
+  // Modify the print value when object is invoked
+  // with Number and String eg Number(instance), String(instance)
   [Symbol.toPrimitive](val) {
     if (val === 'number') return this._size;
     if (val === 'string') return this._location;
