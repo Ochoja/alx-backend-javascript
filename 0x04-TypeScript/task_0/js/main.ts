5f2a689f360b1ab78569b06df4ae374c5407ea9a
Daniel Ochoja <danielochoja@yahoo.com>
2024-01-12 03:54:41 +0100

    updated

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -24,17 +24,40 @@ const studentsList: Student[] = [student1, student2];
 let body = document.body;
 let table = document.createElement('table');
 let table_head = document.createElement('thead');
-let table_row = document.createElement('tr');
+let table_body = document.createElement('tbody');
+let thead_row = document.createElement('tr');
 
-// Define Table Head
+/*Table head definition */
 for (let key in studentsList[0]) {
   let th = document.createElement('th');
   let text = document.createTextNode(key);
   th.appendChild(text);
-  table_row.appendChild(th);
+  thead_row.appendChild(th);
 }
 
-table_head.appendChild(table_row);
+// Add table head to table
+table_head.appendChild(thead_row);
 table.appendChild(table_head);
-table.setAttribute('border', '2');
+/* End table head definition*/
+
+/* Table body definition */
+for (let student of studentsList) {
+  let tr = document.createElement('tr'); // create row for each student
+
+  // loop through student data and add to row
+  for (const [key, value] of Object.entries(student)) {
+    const text = document.createTextNode(value);
+    let td = document.createElement('td');
+    td.appendChild(text);
+    tr.appendChild(td);
+  }
+
+  // add row to table
+  table_body.appendChild(tr);
+}
+/* End table body definition */
+
+// Add table to document
+table.setAttribute('border', '2'); // set value of table border to 2
+table.appendChild(table_body);
 body.appendChild(table);
